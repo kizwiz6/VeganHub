7e0b833ce0a59141dff51b9b3dab618844c71ef8
Kieran Emery <kizwiz@hotmail.co.uk>
2024-11-06 20:08:36 +0000

    Update the routes in App.tsx to include the CreateRecipe page.

--- frontend/vegan-hub-client/src/App.tsx
+++ frontend/vegan-hub-client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import CreateRecipe from './pages/CreateRecipe';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/recipes/new" element={<CreateRecipe />} />
         </Routes>
       </div>
     </Router>
